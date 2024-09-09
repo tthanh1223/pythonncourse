ae901a2b277556cd643ac7bf73917d65ed0abfc1
tthanh1223 <lqd.tk23.vothaithanh@gmail.com>
2024-09-09 20:48:28 +0700

    not thing

--- ex_md2/ex.py
+++ ex_md2/ex.py
@@ -1,1 +1,13 @@
 import re
+file = input("Enter filename:")
+if len(file) < 1:
+    file = 'text1.txt'
+handle = open(file)
+S = 0
+for line in handle:
+    number = re.findall('[0-9]+' ,line)
+    for num in number:
+        num = int(num)
+        S += num
+print(S)
+handle.close()
